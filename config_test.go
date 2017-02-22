f72db3fc2d7bd395ed3096833ff5b912b7f52c49
Ed Marshall <esm@logic.net>
2017-02-21 20:11:06 -0500

    Add basic unit tests for config.go

---
+++ config_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestConfigListenAddress(t *testing.T) {
+	if _, err := LoadConfig(strings.NewReader("Tokens = [\"a\", \"b\"]\n")); err != nil {
+		t.Error("Error parsing TOML configuration:", err)
+	}
+}
+
+func TestConfigTokens(t *testing.T) {
+	if _, err := LoadConfig(strings.NewReader("ListenAddress = \"1.2.3.4\"\n")); err != nil {
+		t.Error("Error parsing TOML configuration:", err)
+	}
+}
+
+func TestConfigAsyncResponse(t *testing.T) {
+	if _, err := LoadConfig(strings.NewReader("AsyncResponse = true\n")); err != nil {
+		t.Error("Error parsing TOML configuration:", err)
+	}
+}
+
+func TestConfigHTTPClientTimeout(t *testing.T) {
+	config, err := LoadConfig(strings.NewReader("HTTPClientTimeout = 10\n"))
+	if err != nil {
+		t.Error("Error parsing TOML configuration:", err)
+	}
+	if config.HTTPClientTimeout != 10*time.Second {
+		t.Errorf("Timeout incorrectly converted: 10 -> %d", config.HTTPClientTimeout)
+	}
+}
